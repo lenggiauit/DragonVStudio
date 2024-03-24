7449b95fb9c5233470509eeb02b0c4172c48a978
Lenggiauit <lenggiauit@gmail.com>
2024-03-24 14:48:03 +0700

    dragon v web

--- dragonvstudio-web/app/termsOfService/page.tsx
+++ dragonvstudio-web/app/termsOfService/page.tsx
@@ -1,6 +1,5 @@
 import React, { ReactElement } from 'react'
 import Footer from '../../components/footer'
-import Layout from '../../components/layout'
 import { Metadata, ResolvingMetadata } from 'next'
 import { AppSetting } from '@/types/type'
 let appSetting: AppSetting = require('../../appSetting.json')
@@ -28,26 +27,23 @@ export async function generateMetadata(
 const TermOfServicePage: React.FC = (): ReactElement => {
   return (
     <>
-      <Layout isPublic={true}>
-        <section className='section'>
-          <div className='container'>
-            <header className='section-header'>
-              <h2>Terms of Service</h2>
-              <hr />
-            </header>
-            <div className='row gap-y text-center'>
-              <p className='lead'>
-                Terms of service are the legal agreements between a service
-                provider and a person who wants to use that service. The person
-                must agree to abide by the terms of service in order to use the
-                offered service. Terms of service can also be merely a
-                disclaimer, especially regarding the use of websites
-              </p>
-            </div>
+      <section className='section'>
+        <div className='container'>
+          <header className='section-header'>
+            <h2>Terms of Service</h2>
+            <hr />
+          </header>
+          <div className='row gap-y text-center'>
+            <p className='lead'>
+              Terms of service are the legal agreements between a service
+              provider and a person who wants to use that service. The person
+              must agree to abide by the terms of service in order to use the
+              offered service. Terms of service can also be merely a disclaimer,
+              especially regarding the use of websites
+            </p>
           </div>
-        </section>
-        <Footer />
-      </Layout>
+        </div>
+      </section>
     </>
   )
 }
